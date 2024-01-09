c49c27f8eb2f961a9e57b519331c8c6fd0afcdf2
syedwaleednawaz000 <syedwaleednawaz333@gmail.com>
2024-01-09 16:20:39 +0500

    by waleed fixes every thing call and post and timer

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:isolate_project/provider.dart';
+import 'package:provider/provider.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ApiProvider(),
+      child: MaterialApp(
+        title: 'Flutter Isolate Example',
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Isolate Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Consumer<ApiProvider>(
+              builder: (context, apiProvider, child) {
+              return             ElevatedButton(
+                onPressed: () {
+                  if(apiProvider.timerValue == 0){
+                    // Start the timer when the button is pressed
+                    context.read<ApiProvider>().startTimer();
+                  }else{
+                    // Start the timer when the button is pressed
+                    context.read<ApiProvider>().stopTimer();
+                  }
+                },
+                child: Text('Start Timer'),
+              );
+            },),
+            Consumer<ApiProvider>(
+              builder: (context, apiProvider, child) {
+                if (apiProvider.timerValue == null) {
+                  return Text('Press the button to start the timer.');
+                } else {
+                  return Text('Timer Value: ${apiProvider.timerValue}');
+                }
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                context.read<ApiProvider>().fetchData();
+              },
+              child: Text('Fetch Data'),
+            ),
+            Consumer<ApiProvider>(
+              builder: (context, apiProvider, child) {
+                if (apiProvider.apiResponse.isEmpty) {
+                  return Text('Press the button to fetch data.');
+                } else {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: apiProvider.apiResponse.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(apiProvider.apiResponse[index]),
+                        );
+                      },
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/provider.dart
@@ -0,0 +1,154 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:isolate';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class ApiProvider extends ChangeNotifier {
+  static const String _apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  List<String> _apiResponse = [];
+  List<String> get apiResponse => _apiResponse;
+
+  Future<void> fetchData() async {
+    ReceivePort receivePort = ReceivePort();
+    await Isolate.spawn(_fetchDataIsolate, receivePort.sendPort);
+
+    SendPort sendPort = await receivePort.first;
+    List<String> result = await _sendReceive(sendPort, 'fetchData');
+
+    _apiResponse = result;
+    notifyListeners();
+  }
+
+  static void _fetchDataIsolate(SendPort sendPort) async {
+    ReceivePort receivePort = ReceivePort();
+    sendPort.send(receivePort.sendPort);
+
+    await for (var msg in receivePort) {
+      SendPort replyTo = msg[0];
+      String command = msg[1];
+
+      if (command == 'fetchData') {
+        List<String> data = await _fetchData();
+        replyTo.send(data);
+      }
+    }
+  }
+
+  static Future<List<String>> _fetchData() async {
+    final response = await http.get(Uri.parse(_apiUrl));
+    if (response.statusCode == 200) {
+      dynamic decodedData = jsonDecode(response.body);
+
+      // Check if the decoded data is a list
+      if (decodedData is List) {
+        List<String> result = decodedData.map((item) => item.toString()).toList();
+        return result;
+      } else {
+        throw Exception('Invalid data type. Expected List.');
+      }
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  Future<List<String>> _sendReceive(SendPort sendPort, String command) async {
+    ReceivePort response = ReceivePort();
+    sendPort.send([response.sendPort, command]);
+
+    dynamic result = await response.first;
+
+    if (result is List<String>) {
+      return result;
+    } else {
+      throw Exception('Invalid response type');
+    }
+  }
+//Todo by posting
+  Future<void> postData(Map<String, dynamic> data) async {
+    ReceivePort receivePort = ReceivePort();
+    await Isolate.spawn(_postDataIsolate, [receivePort.sendPort, data]);
+
+    SendPort sendPort = await receivePort.first;
+    List<String> result = await _sendReceive(sendPort, 'postData');
+
+    _apiResponse = result;
+    notifyListeners();
+  }
+
+  static void _postDataIsolate(List<dynamic> args) async {
+    SendPort sendPort = args[0];
+    Map<String, dynamic> data = args[1];
+
+    ReceivePort receivePort = ReceivePort();
+    sendPort.send(receivePort.sendPort);
+
+    await for (var msg in receivePort) {
+      SendPort replyTo = msg[0];
+      String command = msg[1];
+
+      if (command == 'postData') {
+        List<String> response = await _postData(data);
+        replyTo.send(response);
+      }
+    }
+  }
+
+  static Future<List<String>> _postData(Map<String, dynamic> data) async {
+    final response = await http.post(
+      Uri.parse(_apiUrl),
+      body: jsonEncode(data),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 201) {
+      dynamic decodedData = jsonDecode(response.body);
+
+      // Check if the decoded data is a list
+      if (decodedData is List) {
+        List<String> result = decodedData.map((item) => item.toString()).toList();
+        return result;
+      } else {
+        throw Exception('Invalid data type. Expected List.');
+      }
+    } else {
+      throw Exception('Failed to post data');
+    }
+  }
+
+  //Todo timer value
+  int _timerValue = 0;
+  int get timerValue => _timerValue;
+
+  late Isolate _isolate;
+  late ReceivePort _receivePort;
+
+  void startTimer() async {
+    _receivePort = ReceivePort();
+    _isolate = await Isolate.spawn(_timerIsolate, _receivePort.sendPort);
+    _receivePort.listen((message) {
+      if (message is int) {
+        _timerValue = message;
+        notifyListeners();
+      }
+    });
+  }
+
+  static void _timerIsolate(SendPort sendPort) {
+    int timerValue = 0;
+
+    const duration = Duration(milliseconds: 1);
+    Timer.periodic(duration, (Timer timer) {
+      timerValue += 1;
+      sendPort.send(timerValue);
+    });
+  }
+
+  void stopTimer() {
+    _isolate.kill(priority: Isolate.immediate);
+    _receivePort.close();
+    _timerValue = 0;
+    notifyListeners();
+  }
+}
